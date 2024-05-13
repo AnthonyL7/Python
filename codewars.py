daad50878b938f2208d4381b4ccd77255cd5c9a7
AnthonyL7 <anthonylanda022@gmail.com>
2024-05-13 08:13:44 -0700

    Start completing challenges in codewars

---
+++ codewars.py
@@ -0,0 +1,5 @@
+# Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+def remove_exclamation_marks(s):
+    new_string = s.replace("!", "")
+    
+remove_exclamation_marks("Hello World!")
