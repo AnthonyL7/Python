d69852571921b1a0e2563871d498a3ef88c2c466
AnthonyL7 <anthonylanda022@gmail.com>
2024-05-22 12:08:59 -0700

    Convert a boolean to a string

--- codewars.py
+++ codewars.py
@@ -1,12 +1,9 @@
-#Create a function which answers the question "Are you playing banjo?".
-#If your name starts with the letter "R" or lower case "r", you are playing banjo!
-#The function takes a name as its only argument, and returns one of the following strings:
+#convert a Boolean to a string
 
 
-def are_you_playing_banjo(name):
-    if name.startswith(("R", "r")):
-        print(name + " plays banjo")
+def boolean_to_string(b):
+    if b==True:
+        return 'True'
     else: 
-        print(name + " does not play banjo")
-
-are_you_playing_banjo("Jim")
+        return 'False'
+boolean_to_string(True)
