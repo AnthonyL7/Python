0485f3a9e3e2351c1ca2820d6a3395bd8f78983a
AnthonyL7 <anthonylanda022@gmail.com>
2024-05-22 18:12:07 -0700

    Double the value in the list

--- codewars.py
+++ codewars.py
@@ -1,8 +1,11 @@
-#Return a string's even characters
-def even_chars(st):
-  
-  for i in range(0, len(st)):
-    if i % 2 == 1:
-        print(st[i])
+#Given an array of integers, return a new array with each value doubled.
+def maps(a):
+      #using a list comprehension to display a list
+      print([i * 2 for i in a])
+      
+      #using a for loop to display each value doubled
+      for i in a:
+          multi = i * 2
+          print(multi)
 
-even_chars("abcdefghijklm")
+maps([-1,0,2,3,4,5,6,7,8,9,10])
