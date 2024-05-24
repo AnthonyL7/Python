54a5cc1ef79e9735b0d063b5269db6cdf0709a9d
AnthonyL7 <anthonylanda022@gmail.com>
2024-05-24 10:24:50 -0700

    Convert a string to a list

--- codewars/codewars.py
+++ codewars/codewars.py
@@ -1,5 +1,9 @@
-years_code_list = ['1','2','3','4','2','1','3','2','5','6','7','8','9','10','12','14','15','21','13','20', 'More than 50 years']
-years_code_cleaned = [int(years) for years in years_code_list if years != 'More than 50 years']
+# Convert a string to an array
 
-junior_developers = len([dev for dev in years_code_cleaned if dev < 5])
-print(junior_developers)
+def string_to_array(s):
+    if s:
+      print(s.split())
+    else:
+      print(s.split(","))
+
+string_to_array("")
