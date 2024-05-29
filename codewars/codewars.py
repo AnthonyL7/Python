2bb0e2d740e045151d3311de003bc74e224adfa9
AnthonyL7 <anthonylanda022@gmail.com>
2024-05-29 07:30:49 -0700

    Work on problems

--- codewars/codewars.py
+++ codewars/codewars.py
@@ -1,23 +1,7 @@
-# You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
-#Implement the function which takes an array containing the names of people that like an item.
-
-def likes(names):
-  count = 0
-  #Create conditional statement to show that no one likes this
-  if not names:
-    print('no one likes this')
-  
-  #Create elif conditional statements to show that one more people like it
-  elif len(names) == 1:
-    print(F"{names[0]} likes this")
-  elif len(names) == 2:
-    print(F"{names[0]} and {names[1]} like this")
-  elif len(names) == 3:
-    print(F"{names[0]}, {names[1]} and {names[2]} like this")
-  elif len(names) >= 4:
-    index = 1
-    sub_list = names[index + 1:]
-    count = len(sub_list)
-    print(F"{names[0]}, {names[1]} and {count} others like this")
-
-likes(['Brian J. Mason', 'Nigel', 'Macky Stingray', 'Nene Romanova', 'Galatea', 'Sylia Stingray', 'Daley Wong', 'Priscilla S. Asagiri'])
+def sum_array(a):
+    if a: 
+      print(sum(a))
+    else: 
+       print(0)
+    
+sum_array(range(101))
--- hello.py
+++ hello.py
@@ -1,24 +1,6 @@
-userInput = int(input("Write a year date:"))
-print("The entered number is:", userInput)
+def sort(list):
+  print(list)
+  my_list = [nums for nums in sorted(list)]
+  print(my_list)
 
-year = userInput
-
-if (year % 4 == 0) and (year % 100 != 0):
-  print("its a leap year")
-else:
-  print("Its not a leap year")
-if (year % 400 == 0):
-  print("Its not a leap year")
-
-
-"""if (year % 4 == 0):
-  if (year % 100 == 0):
-      if (year % 400 == 0):
-         print("Its a leap year")
-      else:
-        print("Not a leap year")
-  else:
-      print("Its a leap year")
-else: 
-  print("Not a leap year") 
-"""
+sort([3,1,4,1,5,9,2,6,5])
