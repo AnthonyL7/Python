5d12c4105ac12f53708f872a7988991f0be01ab0
AnthonyL7 <anthonylanda022@gmail.com>
2024-06-26 20:22:42 -0700

    Write better code to pass all tests

--- codewars/codewars.py
+++ codewars/codewars.py
@@ -1,17 +1,16 @@
 #The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
 
 def duplicate_encode(word):
-    duplicates = ''
-    result = ''
-    for letter in word.lower():
-        if word.count(letter) > 1:
-            result += ')'
+    word = word.lower()
+    string = ''
+    result = {}
+    for letter in word:
+        result[letter] = result.get(letter, 0) + 1
+    for letter in word: 
+        if result[letter] > 1:
+            string += ")"
         else:
-            result += '('
-        if letter not in duplicates:
-            duplicates += letter
-    print(result)
-    return duplicates
-    pass
+            string += "("
+    print(string)
   
-duplicate_encode('Success')
+duplicate_encode('OGUF@hPwIZ(gf)Y(ULz pSy@bUseWhJRAsuD)ikc')
