68675c3ae57e4a3c7debd19f141ad108a369c12d
AnthonyL7 <anthonylanda022@gmail.com>
2024-06-26 20:11:06 -0700

    Duplicate encoder

--- codewars/codewars.py
+++ codewars/codewars.py
@@ -1,10 +1,17 @@
-#Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+#The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
 
-def solution(text, ending):
-    if text.endswith(ending):
-        print(True)
-    else:
-        print(False)
-
-
-solution('samurai', 'ra')
+def duplicate_encode(word):
+    duplicates = ''
+    result = ''
+    for letter in word.lower():
+        if word.count(letter) > 1:
+            result += ')'
+        else:
+            result += '('
+        if letter not in duplicates:
+            duplicates += letter
+    print(result)
+    return duplicates
+    pass
+  
+duplicate_encode('Success')
