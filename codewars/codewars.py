5845e1d1badcee931ba463e05dafbd0c87dc06f2
AnthonyL7 <anthonylanda022@gmail.com>
2024-06-28 16:58:43 -0700

    Regex validate PIN code

--- codewars/codewars.py
+++ codewars/codewars.py
@@ -1,21 +1,9 @@
-#Complete the function so that it finds the average of the three scores passed to it and returns the letter value associated with that grade.
-def get_grade(s1, s2, s3):
-    A = 'A'
-    B = 'B'
-    C = 'C'
-    D = 'D'
-    F = 'F'
-    avg = (s1+s2+s3)/3
-    if 90 <= avg <= 100:
-        print(A)
-    elif 80 <= avg < 90:
-        print(B)
-    elif 70 <= avg < 80:
-        print(C)
-    elif 60 <= avg < 70:
-        print(D)
-    elif 0 <= avg < 60:
-        print(F)
+def validate_pin(pin):
+    #return true or false
+  if len(pin) == 4 and pin.isdigit() or len(pin) == 6 and pin.isdigit(): 
+    print(True)
+  else: 
+    print(False)
 
-get_grade(100,100,100)
+validate_pin('-12345')
 
