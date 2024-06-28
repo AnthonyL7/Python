7c06f195824247fed4be548856806b345e6c01f6
AnthonyL7 <anthonylanda022@gmail.com>
2024-06-28 16:37:58 -0700

    Find the average of the tests

--- codewars/codewars.py
+++ codewars/codewars.py
@@ -1,16 +1,21 @@
-#The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+#Complete the function so that it finds the average of the three scores passed to it and returns the letter value associated with that grade.
+def get_grade(s1, s2, s3):
+    A = 'A'
+    B = 'B'
+    C = 'C'
+    D = 'D'
+    F = 'F'
+    avg = (s1+s2+s3)/3
+    if 90 <= avg <= 100:
+        print(A)
+    elif 80 <= avg < 90:
+        print(B)
+    elif 70 <= avg < 80:
+        print(C)
+    elif 60 <= avg < 70:
+        print(D)
+    elif 0 <= avg < 60:
+        print(F)
 
-def duplicate_encode(word):
-    word = word.lower()
-    string = ''
-    result = {}
-    for letter in word:
-        result[letter] = result.get(letter, 0) + 1
-    for letter in word: 
-        if result[letter] > 1:
-            string += ")"
-        else:
-            string += "("
-    print(string)
-  
-duplicate_encode('OGUF@hPwIZ(gf)Y(ULz pSy@bUseWhJRAsuD)ikc')
+get_grade(100,100,100)
+
