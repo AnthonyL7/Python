49cc3228dd4f64722b08092d71f0fb0d9ba2cd6f
AnthonyL7 <anthonylanda022@gmail.com>
2024-07-01 16:27:31 -0700

    Push what I have so far

--- codewars/codewars.py
+++ codewars/codewars.py
@@ -1,6 +1,12 @@
-def DNA_strand(dna):
-    translation = str.maketrans('ATCG', 'TAGC')
-    mod_dna = dna.translate(translation)
-    print(mod_dna)
+def xo(s):
+    s = s.lower()
+    char = 'xo'
+    if 'x' not in s or 'o' not in s:
+        print(False)
+    for i in char:
+        count = s.count(i)
+        if count % 2 != 0:
+            print(False)
+    print(True)     
 
-DNA_strand('AAAA')
+xo('zzoo')
