10452e5f89bddc8e296640f7d6af43c36cbf6540
AnthonyL7 <anthonylanda022@gmail.com>
2024-07-02 15:06:59 -0700

    start working on logic

--- codewars/codewars.py
+++ codewars/codewars.py
@@ -1,13 +1,20 @@
-def odd_ones_out(numbers):
-  dict = {}
-  result = []
-  for i in numbers:
-    dict[i] = dict.get(i,0)+1
+"""If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
-  for i in numbers:
-    if dict[i] % 2 == 0:
-      result.append(i)
-  print(result)
+Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+
+Additionally, if the number is negative, return 0.
+
+Note: If the number is a multiple of both 3 and 5, only count it once."""
+
+
+
+def solution(number):
+    for i in range(1, number):
+      if i % 3 == 0 or i % 5 == 0:
+        sum += 1
+        print(sum)
+      elif i == -1:
+         return 0
+    pass
   
-
-odd_ones_out([1, 1, 2, 2, 3, 3, 3])
+solution(6)
