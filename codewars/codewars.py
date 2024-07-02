a6c165a41a88728412effa1f679768baef0bef67
AnthonyL7 <anthonylanda022@gmail.com>
2024-07-01 17:00:07 -0700

    Find whether the string has letters

--- codewars/codewars.py
+++ codewars/codewars.py
@@ -1,17 +1,4 @@
-def xo(s):
-    s = s.lower()
-    char = 'xo'
-    count_x = s.count('x')
-    count_o = s.count('o')
-    if 'x' not in s and 'o' not in s:
-        print(True)
-    if 'x' in s and 'o' not in s:
-        print(False)
-    if 'x' not in s and 'o' in s:
-        print(False)
-    if count_x % 2 != 0 or count_o % 2 != 0 and count_x == 0 or count_o == 0:
-        print(False)
-    return True
-   
+def is_it_letter(s):
+  print(s.isalpha())
 
-xo('zpzpzpp')
+is_it_letter('A')
