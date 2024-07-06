77df733a29553b1866cbb049a696e5bbc98d2ff0
AnthonyL7 <anthonylanda022@gmail.com>
2024-07-05 18:58:41 -0700

    Figured out regular test cases

--- codewars/codewars.py
+++ codewars/codewars.py
@@ -1,21 +1,11 @@
-"""If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
-Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+def number(lines):
+  if isinstance(lines, list):
+    add = ["1: ", "2: ", "3: " ]
+    lst = zip(add, lines)
+    new_list = [num + char for num,char in lst]
+    print(new_list)
+  else:
+    return []
 
-Additionally, if the number is negative, return 0.
-
-Note: If the number is a multiple of both 3 and 5, only count it once."""
-
-
-
-def solution(number):
-    sum = 0
-    for i in range(1, number):
-      if i % 3 == 0 or i % 5 == 0:
-         sum += i
-      elif i == -1:
-         return 0
-    print(sum)
-    pass
-  
-solution(20)
+number(["a", "b", "c"])
