9b3284497cc81c2ab6fe556066caf96adbccce91
AnthonyL7 <anthonylanda022@gmail.com>
2024-07-16 12:28:20 -0700

    5 Kyuextract the domain name from the url

--- codewars/codewars.py
+++ codewars/codewars.py
@@ -1,8 +1,17 @@
-def descending_order(num):
-  string = str(num)
-  sorted_string = ''.join(sorted(string, reverse=True))
-  number = int(sorted_string)
-  print(number)
-  pass
 
-descending_order(65321000)
+
+def domain_name(url):
+  if url.startswith('http://'):
+    url = url[len("http://"):]
+  elif url.startswith("https://"):
+    url = url[len("https://"):]
+  
+  if url.startswith("www."):
+    url = url[len("www."):]
+  
+  domain_part = url.split("/")[0]
+  domain = domain_part.split(".")[0]
+
+  print(domain_part)
+
+domain_name("www.xakep.ru")
