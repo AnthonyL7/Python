30251e6ff67006f93a11f662c5afad15d7510406
AnthonyL7 <anthonylanda022@gmail.com>
2024-07-19 13:53:23 -0700

    Add aREADME.md file to write down thinking process.

--- codewars/codewars.py
+++ codewars/codewars.py
@@ -1,11 +1,24 @@
-def open_or_senior(data):
-  lst = []
-  for age, score in data:
-    if age >= 55 and score > 7:
-      lst.append("Senior")
-    else:
-      lst.append("Open")
-  print(lst)
-  pass
+def is_valid_walk(walk):
 
-open_or_senior([(16, 23),(73,1),(56, 20),(1, -1)])
+    ns_count = 0
+    ew_count = 0
+    if len(walk) > 10 or len(walk) < 10:
+       print(False)
+    else:
+      for i in walk:
+        if i == 'n':
+            ns_count += 1
+        elif i == 's':
+            ns_count -= 1
+        elif i == 'e':
+            ew_count += 1
+        elif i == 'w':
+            ew_count -= 1
+      if ns_count == 0 and ew_count == 0:
+        print(True)
+      else:
+        print(False)
+      
+    
+
+is_valid_walk(['n', 's', 'e', 'w', 'n', 's', 'n', 'w', 'n', 's'])
