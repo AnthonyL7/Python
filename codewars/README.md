6f9e2c5710f219bb5c0aeae07b935b7c5e15f658
AnthonyL7 <anthonylanda022@gmail.com>
2024-07-19 14:19:20 -0700

    6 Kyu

---
+++ codewars/README.md
@@ -0,0 +1,4 @@
+July 19, 2024
+6 Kyu Break camelCase
+
+Use re module to manipulate string to find Capital letters and add spaces in between them
--- codewars/codewars.py
+++ codewars/codewars.py
@@ -1,24 +1,8 @@
-def is_valid_walk(walk):
-
-    ns_count = 0
-    ew_count = 0
-    if len(walk) > 10 or len(walk) < 10:
-       print(False)
-    else:
-      for i in walk:
-        if i == 'n':
-            ns_count += 1
-        elif i == 's':
-            ns_count -= 1
-        elif i == 'e':
-            ew_count += 1
-        elif i == 'w':
-            ew_count -= 1
-      if ns_count == 0 and ew_count == 0:
-        print(True)
-      else:
-        print(False)
-      
+import re
+def solution(s):
     
+    result = re.sub(r'(?<=[a-z])(?=[A-Z])|(?<=[A-Z])(?=[A-Z][a-z])',' ', s)
+    print(result)
 
-is_valid_walk(['n', 's', 'e', 'w', 'n', 's', 'n', 'w', 'n', 's'])
+
+solution("helloWorld")
