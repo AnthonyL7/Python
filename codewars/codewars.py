9b9446268219bd839de6d8fdcbb9d7a871bd63ad
AnthonyL7 <anthonylanda022@gmail.com>
2024-07-19 12:31:40 -0700

    7 Kyu Printer Errors

--- codewars/codewars.py
+++ codewars/codewars.py
@@ -1,7 +1,13 @@
 import re
 
 def printer_error(s):
-  string = bool(re.search('[a-mA-M]', s))
+
+  range_count = 0
+  for char in s:
+    if char not in 'abcdefghijklm':
+      range_count += 1
+  string = f"{range_count}/{len(s)}"
   print(string)
+  
     
-printer_error("aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz")
+printer_error("kkkwwwaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyzuuuuu")
