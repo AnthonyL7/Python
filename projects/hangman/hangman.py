60f998abd86ae274c0e4d21462ab70dcdd3dc7c4
AnthonyL7 <anthonylanda022@gmail.com>
2024-08-09 14:57:14 -0700

    Figure out how to print underscores for every letter in the word

--- projects/hangman/hangman.py
+++ projects/hangman/hangman.py
@@ -1,16 +1,28 @@
 from words import words
+import random
 
-def random():
-  import random
-  if all(isinstance(i, str) for i in words):
-    random_word = random.choice(words)
-    print(random_word)
+#def random_generator():
+if all(isinstance(i, str) for i in words):
+  random_word = random.choice(words)
+  print(random_word)
 
-random()
 
-def game():
+def game(rand):
+  
   count = ""
-  for i in random:
+  for i in rand:
     if type(i) == str:
       count += "_"
   print(count)
+
+game(random_word)
+
+def initiate_game(word):
+  user_input = input("Try to guess the word one letter at a time: ")
+  for i in word: 
+    if user_input.lower() in word: 
+      print(True)
+    else: 
+      print(False)
+
+initiate_game(random_word)
