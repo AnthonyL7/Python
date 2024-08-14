c462f8fb9ca00b39b601639dfc82ae516ec56b07
AnthonyL7 <anthonylanda022@gmail.com>
2024-08-14 15:11:53 -0700

    6 Kyu Your, order, please

--- codewars/README.md
+++ codewars/README.md
@@ -1,5 +1,7 @@
 August 13, 2024
-7 Kyu is this a triangle?
+6 Kyu Your order, please
+
+
 
 
 
--- codewars/codewars.py
+++ codewars/codewars.py
@@ -1,8 +1,12 @@
-def is_triangle(a, b, c):
-  if a > 0 and b > 0 and c > 0:
-    if c + b > a and a + b > c and c + a > b:
-      print(True)
-  print(False)
-  pass
+import re
 
-is_triangle(7,2,2)
+def order(sentence):
+  words = sentence.split()
+  match = re.search(r'\d+', sentence)
+  sorted_words = sorted(words, key=lambda word: int(re.search(r'\d+', word).group()))
+
+  sorted_sentence = ' '.join(sorted_words)
+  print(sorted_sentence)
+
+
+order("is2 Thi1s T4est 3a")
