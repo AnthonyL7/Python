804163b8f94e97ab15c985aa52bac1d3bf921e9a
AnthonyL7 <anthonylanda022@gmail.com>
2024-09-24 19:54:31 -0700

    6 kyu Count the smiley faces

--- codewars/codewars.py
+++ codewars/codewars.py
@@ -1,6 +1,15 @@
-def number(bus_stops):
-  result = [i[0] - i[1] for i in bus_stops]
-  print(sum(result))
-  pass
+def count_smileys(arr):
+    valid_eyes = [':', '~', ';']
+    valid_noses = ['-', '~']
+    valid_mouths = [')', 'D']
+    count = 0
+    for i in arr:
+        if len(i) == 2:
+            if i[0] in valid_eyes and i[1] in valid_mouths:
+                count += 1
+        elif len(i) == 3:
+            if i[0] in valid_eyes and i[1] in valid_noses and i[2] in valid_mouths:
+                count += 1
+    print(count)
 
-number([[3,0],[9,1],[4,10],[12,2],[6,1],[7,10]])
+count_smileys([':D',':~)',';~D',':)'])
