a1924e9ec24df4a26a557ef31d40b4ace6067ba6
AnthonyL7 <anthonylanda022@gmail.com>
2024-09-28 17:46:21 -0700

    8 kyu

--- codewars/codewars.py
+++ codewars/codewars.py
@@ -1,15 +1,5 @@
-def count_smileys(arr):
-    valid_eyes = [':', '~', ';']
-    valid_noses = ['-', '~']
-    valid_mouths = [')', 'D']
-    count = 0
-    for i in arr:
-        if len(i) == 2:
-            if i[0] in valid_eyes and i[1] in valid_mouths:
-                count += 1
-        elif len(i) == 3:
-            if i[0] in valid_eyes and i[1] in valid_noses and i[2] in valid_mouths:
-                count += 1
-    print(count)
+def smash(words):
+    return ' '.join([str(i) for i in words])
 
-count_smileys([':D',':~)',';~D',':)'])
+
+smash(["this", "is", "a", "really", "long", "sentence"])
