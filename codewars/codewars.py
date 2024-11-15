3f2576fff4275e93f57636218b533adacdbef7f6
AnthonyL7 <anthonylanda022@gmail.com>
2024-11-14 20:24:34 -0800

    8 KYU - Correct the mistakes of the character recognition software

--- codewars/codewars.py
+++ codewars/codewars.py
@@ -1,7 +1,7 @@
-def set_alarm(employed, vacation):
-  if employed == True and vacation == False:
-    return True
-  else:
-    return False
+def correct(s):
+  if '0' in s or '5' in s or '1' in s:
+    s1 = s.replace("0","O").replace('5', 'S').replace('1','I')
+    print(s1)
   pass
-set_alarm(True, False)
+
+correct("DUBL1N")
