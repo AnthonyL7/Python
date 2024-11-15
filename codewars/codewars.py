b4a1de5d4525add4149d67aed4de25d9dc0ff6f1
AnthonyL7 <anthonylanda022@gmail.com>
2024-11-14 20:10:05 -0800

    8 KYU, L1: Set Alarm

--- codewars/codewars.py
+++ codewars/codewars.py
@@ -1,12 +1,7 @@
-def get_sum(a,b):
-  if a == b:
-    return a
-  
-  start = min(a,b)
-  end = max(a,b)
-  total = 0 
-  total = sum(range(a, b+1))
-  print(total)
-    
-
-get_sum(0,-1)
+def set_alarm(employed, vacation):
+  if employed == True and vacation == False:
+    return True
+  else:
+    return False
+  pass
+set_alarm(True, False)
