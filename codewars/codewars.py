2c5a686d2907e7415f8f85e54b82a40e71872572
AnthonyL7 <anthonylanda022@gmail.com>
2025-03-01 15:09:40 -0800

    8 KYU Add all positive and negative numbers

--- codewars/codewars.py
+++ codewars/codewars.py
@@ -1,7 +1,15 @@
-def correct(s):
-  if '0' in s or '5' in s or '1' in s:
-    s1 = s.replace("0","O").replace('5', 'S').replace('1','I')
-    print(s1)
-  pass
-
-correct("DUBL1N")
+def count_positives_sum_negatives(arr):
+  positive_sum = 0
+  negative_sum = 0
+  empty = []
+  for i in arr:
+    if i > 0:
+      positive_sum += i
+    elif i < 0:
+      negative_sum += i
+    else:
+      print(empty)
+  empty.insert(0, positive_sum)
+  empty.append(negative_sum)
+  print(empty)
+count_positives_sum_negatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15])
