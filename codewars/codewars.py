375e28c730139091c366da4a090682d72ff409d2
AnthonyL7 <anthonylanda022@gmail.com>
2025-03-09 12:51:40 -0700

    8 Kyu - Check for factor

--- codewars/codewars.py
+++ codewars/codewars.py
@@ -1,14 +1,9 @@
-def invert(lst):
-  empty = []
-  append = 0
-  if lst is []:
-     print(empty)
-  else:
-    for i in lst:
-      if i > 0:
-        append = i * -1
-      else:
-        append = i * -1
-      empty.append(append)
-    print(empty)
-invert([1,-2,3,-4,5])
+def check_for_factor(base, factor):
+    if base%factor == 0:
+        print('true')
+        return True
+    else:
+        print('false')
+        return False
+
+check_for_factor(10,2)
