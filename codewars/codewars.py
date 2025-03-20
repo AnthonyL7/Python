dea996dd020b3197123c2099b77be8d43fb829be
AnthonyL7 <anthonylanda022@gmail.com>
2025-03-20 14:59:05 -0700

    8 Kyu Exclusive Or

--- codewars/codewars.py
+++ codewars/codewars.py
@@ -1,7 +1,4 @@
-def sequence_sum(begin_number, end_number, step):
-    if begin_number > end_number:
-      return 0 
-    else:
-      total = sum(range(begin_number, end_number+1, step))
-      return total
-sequence_sum(1,5,3)
+def xor(a,b):
+  print(a^b)
+
+xor(True, True)
