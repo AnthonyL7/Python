97312e09710eb86f087aa6b54971457f7158f110
AnthonyL7 <anthonylanda022@gmail.com>
2025-03-23 10:42:54 -0700

    8 Kyu Sum without highest and lowest number

--- codewars/codewars.py
+++ codewars/codewars.py
@@ -1,4 +1,12 @@
-def xor(a,b):
-  print(a^b)
+def sum_array(arr):
+  if not isinstance(arr, list) or len(arr) < 3:
+    print('0')
 
-xor(True, True)
+  arr_copy = arr.copy()
+  arr_copy.remove(max(arr_copy))
+  arr_copy.remove(min(arr_copy))
+
+  total = sum(arr_copy)
+  print(total)
+
+sum_array([1, 1, 11, 2, 3])
