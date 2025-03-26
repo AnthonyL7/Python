0e6e267a8ca5b3f76f6ed0feeaa270ea41159e4b
AnthonyL7 <anthonylanda022@gmail.com>
2025-03-26 10:38:50 -0700

    Password Validator 7 Kyu

--- codewars/codewars.py
+++ codewars/codewars.py
@@ -1,5 +1,20 @@
-def friend(x):
-  result = [name for name in x if len(name) == 4]
+def password(st):
+  has_lower = False
+  has_upper = False
+  is_digit = False
+  if len(st) >= 8:
+    for char in st:
+      if char.islower():
+        has_lower = True
+      elif char.isupper():
+        has_upper = True
+      elif char.isdigit():
+        is_digit = True
+    if has_lower and has_upper and is_digit:
+        print('True')
+    else:
+      print('False')
+  else:
+    print('False')
 
-  print(result)
-friend(["Ryan", "Kieran", "Jason", "Yous"])
+password('abcd1234')
